3dc6dd4a4785ac17b074a5feaef6f5653c4a06b4
Salvador Federico Milanes Braniff <salvadormilanesbraniff@gmail.com>
2024-11-16 14:15:31 -0600

    Remove unecessary conditional paths

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProduction ? '/burningboats.github.io' : '',
-  assetPrefix: isProduction ? '/burningboats.github.io' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
