3d9fcf4b06cfd243cf46efa40d5807c498c1f010
Salvador Federico Milanes Braniff <salvadormilanesbraniff@gmail.com>
2024-11-16 14:15:58 -0600

    Add custom palette (WIP)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme"; // Import defaultTheme
 
 export default {
   content: [
@@ -9,8 +10,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--background)", // Custom CSS variable for background
+        foreground: "var(--foreground)", // Custom CSS variable for foreground
+        bb: {
+          DEFAULT: '#042433'
+        }
+      },
+      fontFamily: {
+        crete: ['"Crete Round"', ...defaultTheme.fontFamily.serif], // Add Crete Round with serif fallback
       },
     },
   },
