b00265a7867f16d8b840ac70c7ed3874c29c1a8d
Salvador Federico Milanes Braniff <salvadormilanesbraniff@gmail.com>
2024-11-16 16:57:40 -0600

    Simplify page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,20 @@
 "use client";
 
-import Image from "next/image";
-import { useEffect, useState } from "react";
-
-function scrollToSection(sectionId: string) {
-  const section = document.getElementById(sectionId);
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
-}
+import Footer from "./_components/footer";
+import Header from "./_components/header";
+import HeroSection from "./_components/hero";
+import ProjectsSection from "./_components/projects";
+import TeamSection from "./_components/team";
 
 export default function Home() {
-  const [showNavbar, setShowNavbar] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-
-      // Show navbar when scrolling up, hide when scrolling down
-      if (currentScrollY > lastScrollY) {
-        setShowNavbar(false); // Hide navbar
-      } else {
-        setShowNavbar(true); // Show navbar
-      }
-
-      setLastScrollY(currentScrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollY]);
-
   return (
-    <div className="scroll-container">
-      <style jsx global>{`
-        .scroll-container {
-          overflow-y: scroll;
-          height: 100vh;
-        }
-        .scroll-container::-webkit-scrollbar {
-          width: 0;
-          background: transparent;
-        }
-        .scroll-container {
-          scrollbar-width: none;
-        }
-        .scroll-container::-webkit-scrollbar-thumb {
-          display: none;
-        }
-      `}</style>
-
-      <header
-        className={`bg-white text-bb shadow-md transform transition-transform duration-300 ${
-          showNavbar ? "translate-y-0" : "-translate-y-full"
-        } sticky top-0 z-10`}
-      >
-        <nav className="container mx-auto flex items-center justify-between py-2 px-4 flex-wrap md:flex-nowrap">
-          <div className="text-lg font-bold">
-            <Image
-              src="/images/logo.svg"
-              width={200}
-              height={200}
-              alt="Company Logo"
-            />
-          </div>
-          <div className="flex space-x-4 md:space-x-6">
-            <button
-              className="text-black hover:text-gray-600 px-4"
-              onClick={() => scrollToSection("section1")}
-            >
-              Home
-            </button>
-            <button
-              className="text-black hover:text-gray-600 px-4"
-              onClick={() => scrollToSection("section2")}
-            >
-              About
-            </button>
-            <button
-              className="text-black hover:text-gray-600 px-4"
-              onClick={() => scrollToSection("section3")}
-            >
-              Contact Us
-            </button>
-          </div>
-        </nav>
-      </header>
-
+    <div className="h-screen overflow-y-auto no-scrollbar">
       <main>
-        <section
-          id="section1"
-          className="h-screen bg-blue-100 flex items-center justify-center"
-        >
-          <h1 className="text-4xl">Welcome to Section 1</h1>
-        </section>
-        <section
-          id="section2"
-          className="h-screen bg-blue-200 flex items-center justify-center"
-        >
-          <h1 className="text-4xl">Welcome to Section 2</h1>
-        </section>
-        <section
-          id="section3"
-          className="h-screen bg-blue-300 flex items-center justify-center"
-        >
-          <h1 className="text-4xl">Welcome to Section 3</h1>
-        </section>
+        <Header />
+        <HeroSection />
+        <ProjectsSection/>
+        <TeamSection/>
+        <Footer />
       </main>
     </div>
   );
