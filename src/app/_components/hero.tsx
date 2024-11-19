85d8e6b15bf14a3c967e18ed4510dd55ef2f90b6
Firefrog88 <A01029956@ITESM.mx>
2024-11-18 20:52:08 -0600

    Add in intro text (placeholder)

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,12 +1,15 @@
 export default function HeroSection() {
   return (
     <section
-      id="hero"
       className="relative h-screen w-screen flex items-center bg-fixed bg-cover bg-center"
       style={{
         backgroundImage: "url('/images/crater.png')", // Replace with your image
       }}
     >
+      <div
+        id="hero"
+        className="absolute -top-24" // Offset equal to navbar height (96px = 24rem)
+      />
       {/* Semi-Transparent Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
@@ -14,7 +17,7 @@ export default function HeroSection() {
       <div className="relative z-2 px-4 font-raleway text-left max-w-lg ml-10">
         <h1 className="text-4xl font-bold mb-4">Our Story</h1>
         <p className="text-sm leading-relaxed">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque bibendum nec ipsum vitae auctor. Vivamus rhoncus vestibulum felis, non maximus tortor gravida nec. Sed id elementum sapien. Nulla vitae lorem sit amet ex aliquet interdum ut ut diam. Proin rutrum fermentum est eu ultricies. Nulla sed neque mauris. Curabitur faucibus urna sit amet ex sagittis, sit amet suscipit leo rutrum. Nulla dictum sit amet orci a sagittis. Mauris in pellentesque est, ut volutpat felis.
+        Burning Boats Studio was founded by nine passionate developers in Mexico City, brought together by a shared love for gaming and creativity. Each of us brings unique skills to the table—programming, design, art, and sound—all driven by the vision to craft immersive and memorable gaming experiences. As a small indie team, we embrace challenges and push boundaries to tell compelling stories and deliver gameplay that resonates with players. Together, we're building a future where exploration, creativity, and fun come alive in every project.
         </p>
       </div>
     </section>
