49e79e9280c8f745fecc08baa597cb6a728732a6
Firefrog88 <A01029956@ITESM.mx>
2024-11-18 20:36:26 -0600

    Further simplify main page content structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import Footer from "./_components/footer";
-import Header from "./_components/header";
 import HeroSection from "./_components/hero";
 import ProjectsSection from "./_components/projects";
 import TeamSection from "./_components/team";
 
 export default function Home() {
   return (
-    <div className="h-screen overflow-y-auto no-scrollbar">
-      <main>
-        <Header />
-        <HeroSection />
-        <ProjectsSection/>
-        <TeamSection/>
-        <Footer />
-      </main>
-    </div>
+    <>
+      <HeroSection />
+      <ProjectsSection/>
+      <TeamSection/>
+    </>
   );
 }
